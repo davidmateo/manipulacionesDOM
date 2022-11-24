faaf248815866ebd0cbea56f099c816fd2ab51b4
david mateo <sanchezdavidmateo03@gmail.com>
2022-11-23 20:43:40 -0500

    adicion de datetask

--- script.js
+++ script.js
@@ -8,30 +8,47 @@ import deleteIcon from "./components/deleteIcon.js";
 
 const btn = document.querySelector('[data-form-btn]');
 
+//add task 
+const addTask = (evento) =>{
+    const list = document.querySelector("[data-list]");
+    const task = createTask(evento)
+    //a list se le agrega un hijo que se  llama task
+    list.appendChild(task);
+
+}
+
 const createTask = (evento) => {
     evento.preventDefault();
     const input = document.querySelector('[data-form-input]');
+    const calendar = document.querySelector('[data-form-date]');
+    const date = calendar.value;
+    const dateFormat = moment(date).format('DD/MM/YYYY');
     const value = input.value;
-    const list = document.querySelector("[data-list]");
     const task = document.createElement("li");
-    task.classList.add("card");
+    task.classList.add("card"); 
     //vacia el input
     input.value = '';
+    calendar.value = null;
     //backticks
 
     const taskContent = document.createElement('div');
+    const dateContent = document.createElement('div');
+    const dateTask = document.createElement('span');
     const titleTask = document.createElement('span');
+    dateTask.classList.add('task');
     titleTask.classList.add('task');
+    dateTask.innerText= dateFormat;
     titleTask.innerText= value;
     taskContent.appendChild(checkComplete());
+    dateContent.appendChild(dateTask);
     taskContent.appendChild(titleTask);
     task.appendChild(taskContent);
+    task.appendChild(dateContent);
     task.appendChild(deleteIcon());
     //task.innerHTML = content;
-    //a list se le agrega un hijo que se  llama task
-    list.appendChild(task);
+    return task;
 };
 
 //capturar el click en el btn
 //tipo listener
-btn.addEventListener("click", createTask);
+btn.addEventListener("click", addTask);
